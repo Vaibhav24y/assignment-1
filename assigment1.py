32b283206c4b41d96aa84a90c38d972a2b24ceb8
Vaibhav24y <118295704+Vaibhav24y@users.noreply.github.com>
2023-06-25 19:15:20 +0530

    Add files via upload

---
+++ assigment1.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the marks "))
+b = int(input("Enter the marks "))
+c = int(input("Enter the marks"))
+result= a+b+c
+avg = result/3
+print("Total marks: ",result)
+print("Average marks: ",avg)
